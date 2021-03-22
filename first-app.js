6ad935fd1b6c8e7bd316a54905332c1fac6f8e5e
github username <github email address>
2021-03-22 15:09:06 -0400

    first-app

--- first-app.js
+++ first-app.js
@@ -1,4 +1,10 @@
 console.log('Hello from Node.js')
 
+const fs = require('fs');
+// ES5 syntax ^
 
 
+fs.writeFileSync('hello.txt', 'Hello from Node.js');
+// This will make text files !
+// First arguemnt is the file name
+// second arguemnt is what's inside that file
