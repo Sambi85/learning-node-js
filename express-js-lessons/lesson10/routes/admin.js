dcf8c7a5c69f148baade8bfecb62edd0844c2d48
github username <github email address>
2021-04-05 16:04:03 -0400

    send-file

---
+++ express-js-lessons/lesson10/routes/admin.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path') //<-- core module for file paths
+const router = express.Router();
+
+router.get('/add-product',(req, res, next) => {
+  res.sendFile(path.join(__dirname,'../','views', 'add-product.html'));
+})
+// Refactor: Deleted form variable and res.send(), add-product.html has us covered!
+
+router.post("/add-product", (req, res, next) => {
+    console.log(req.body);
+    res.redirect("/");
+  });
+
+module.exports = router; 
