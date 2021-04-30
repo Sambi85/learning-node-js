82d6902050901de65dac9835552de4ca9566db32
github username <github email address>
2021-04-30 14:49:38 -0400

    routes-refactor

--- express-js-lessons/lesson14/routes/admin.js
+++ express-js-lessons/lesson14/routes/admin.js
@@ -1,26 +1,10 @@
 const express = require('express');
-const path = require('path')
+const productsController = require('../controllers/products'); //<-- Import here
 const rootDir = require('../util/path') 
 const router = express.Router();
-const products = [];  //<-- instead of DB, we are testing Data flow
 
-router.get('/add-product',(req, res, next) => {
-  res.render('/add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  })
-})
+router.get('/add-product', productsController.getAddProduct); //<-- reference here
+router.post("/add-product", productsController.postAddProduct); //<-- reference here
 
-router.post("/add-product", (req, res, next) => {
-    products.push({ title: req.body.title }) //<--- this enters a new obj to products array
-    res.redirect("/");
-  });
-
-// module.exports = router; //<--- let's refactor
-
-// individually export routes & products here, will refactor on app.js level
-exports.routes = router;
-exports.products = products;
+module.exports = router;
+// exports.products = products; // <--- not needed/ no array
---
+++ express-js-lessons/lesson15/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser'); 
+// const path = require('path') // <-- not needed
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views'); 
+
+const adminRoutes = require('./routes/admin') 
+const shopRoutes = require('./routes/shop') 
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use( (req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found' })  
+});// <--- refactor the middle ware 
+
+
+
+
+
+app.listen(3003);
